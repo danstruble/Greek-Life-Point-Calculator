7bb417a48772ea979a25b8e394679065c34b4414
Dan <dbstruble@gmail.com>
2018-12-13 00:19:39 -0500

    Added statistics reporting for admins and users, added comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import ast
 import pandas as pd
-import matplotlib as plt
 
 # This function checks if the entered username is in either the user or admin file, and assigns a privilege level.
 # This function also uses try excepts to ensure that the program doesn't break if you are missing the required files
 # This will notify the user of the file that they are missing.
+# Coded by Dan and Corey
 def userVerification(enteredUser):
     try:
         ah = open("admins.txt")
@@ -33,6 +33,7 @@ def userVerification(enteredUser):
 
 # This function loads the approved events dictionary from a text file, and does a literal evaluation to turn the string
 # into a dictionary. Also contains a fail safe if file does not exist or contains not a dictionary
+# Coded by Dan and Jon
 def loadApprovedEvents():
     try:
         eh = open ("approvedevents.txt")
@@ -46,12 +47,14 @@ def loadApprovedEvents():
     return approvedEvents
 
 # This function saves the approved event dictionary into a text file by turning it into a string
+# Coded by Dan and Jon
 def saveApprovedEvents():
     with open('approvedevents.txt', 'w+') as file:
         file.write(str(approvedEvents))
 
 # Utilizes same method for approved events to load attended events dictionary from a text file, and evaluate it from a
 # string to a dictionary. Also contains a fail safe if file does not exist or contains not a dictionary
+# Coded by Dan and Jon
 def loadAttendedEvents():
     try:
         eh = open ("attendedevents.txt")
@@ -64,12 +67,14 @@ def loadAttendedEvents():
     return attendedEvents
 
 # Utilizes same save method as above to save the attended dictionary to a text file
+# Coded by Dan and Jon
 def saveAttendedEvents():
     with open('attendedevents.txt', 'w+') as file:
         file.write(str(attendedEvents))
     print("done")
 
 # This function allows you to enter approved event information into the approved events dictionary
+# Coded by Dan and Grace
 def eventEntry():
     finished = False
     while not finished:
@@ -95,6 +100,7 @@ def eventEntry():
             indexEvents()
 
 # This populates the index dictionaries to search for events
+# Coded by Dan, Natalie, and Grace
 def indexEvents():
     nameIndex = {}
     dateIndex = {}
@@ -123,6 +129,7 @@ def indexEvents():
     return [nameIndex,dateIndex]
 
 # This function allows users to search for events by date or search term
+# Coded by Dan, Natalie, Grace, Corey, and Jon
 def searchEvents():
     searchTerm = input("Enter term to search for, or date (mm/dd/yy): ").lower()
     results = []
@@ -146,6 +153,7 @@ def searchEvents():
 # Main intention of this function is to prompt user for event ID, or search for an event using the indexed criteria
 # Which appends the event id to the key value pair associated with the person's name
 # Uses try excepts as an interesting approach to handle different data types being entered in one prompt
+# Coded by Dan, Natalie, and Grace
 def attendedEvent(username):
     finished = False
 
@@ -206,20 +214,50 @@ def attendedEvent(username):
 
 # This function displays attendance stats, allowing admins to search for users or run a report for all users
 # Regular users will be able to see their own stats.
+# Coded by Dan, Natalie, Grace, and Corey
 def displayStats():
-    print("not done")
+
+    # This populates the dictionary with current stats from attendedEvents dictionary
+    statsDict = {}
+
+    for user in attendedEvents:
+        statsDict[user] = len(attendedEvents[user])
+
+    stats = pd.DataFrame.from_dict(statsDict, orient="index", columns=["Events Attended"])
+    stats = stats.sort_values(by="Events Attended", ascending=False)
+
+    if privLevel >= 2:
+        usrInput = input("Do you want 'all' stats or a specific 'user'?: ").lower()
+        if usrInput == "all":
+            print(stats)
+
+        elif usrInput == "user":
+            usrInput = input("Which user do you want to run stats for?: ").title()
+
+            if usrInput in attendedEvents:
+                print("%s attended %s events" %(usrInput, statsDict[usrInput]))
+
+            else:
+                print("Incorrect user, or no recorded events.")
+
+        else:
+            print("Incorrect entry.")
+    elif privLevel >= 1:
+        print("You attended %s events" %(statsDict[username]))
 
 # This function displays commands for the highest privilege level, and displays lower privilege level commands that the
 # user can also use
+# Coded by Dan, Corey, and Jon
 def displayCommands(privLevel):
 
     if privLevel >= 2:
         print("Enter 'event' to create an event")
-        print("Enter 'stats' to view statistics")
     if privLevel >= 1:
         print("Enter 'attend' to enter attendance")
         print("Enter 'search' to search events")
+        print("Enter 'stats' to see statistics")
 
+# Below was coded by Dan, Natalie, and Grace
 # Startup
 
 approvedEvents = loadApprovedEvents()
@@ -229,13 +267,11 @@ print(approvedEvents)
 attendedEvents = loadAttendedEvents()
 done = False
 
-
 print("Welcome to the Greek Life Point Calculator")
 # Login handler
 username = input("Please enter your name: ").title()
 print(username)
 privLevel = userVerification(username)
-
 
 # User input loop
 
@@ -249,7 +285,7 @@ else:
             done = True
         elif usrInput == "event" and privLevel >= 2:
             eventEntry()
-        elif usrInput == "stats" and privLevel >= 2:
+        elif usrInput == "stats" and privLevel >= 1:
             displayStats()
         elif usrInput == "attend" and privLevel >= 1:
             attendedEvent(username)
@@ -257,6 +293,3 @@ else:
             searchEvents()
         else:
             print("Incorrect command or incorrect permission level.")
-
-
-
